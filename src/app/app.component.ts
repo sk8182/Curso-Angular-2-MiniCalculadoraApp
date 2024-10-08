ac0b180216ae1d9953ffd15776af96b98c2b4dfd
Julio Martinez <sk8182enkidu@gmail.com>
2024-10-08 11:48:09 +0200

    Curso Angular. Práctica guiada I. Vídeo 12 -  Elaboramos una mini-calculadora para lo que utilizaremos interpolación, two way binding y algunos conocimientos JavaScript.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,28 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MiniCalculadoraApp';
+  title = 'Mini Calculadora';
+
+  numero1: number = 0;
+  numero2: number = 0;
+  resultado: number = 0;
+
+  suma():void{
+
+    this.resultado=this.numero1+this.numero2;
+
+  }
+
+  resta():void{
+
+    this.resultado=this.numero1-this.numero2;
+
+  }
+
+  multiplica():void{
+
+    this.resultado=this.numero1*this.numero2;
+
+  }
+
 }
